c7437267d90f39dbae178f74f1d6663163df86a2
Monty Saengsavang <montysaengsavang@Montys-MacBook-Pro-2.local>
2018-11-10 18:37:18 -0800

    React initialization project Started react project instead of node

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const express = require('express')
-const cool = require('cool-ascii-faces')
 const path = require('path')
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  var app = express()
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+
+  app.get('/', function (req, res) {
+    res.render('pages/index')
+  })
+
+  app.get('/signup', function (req, res) {
+    res.render('pages/signup')
+  })
+
+  app.post('/signup', function(req, res) {
+    console.log(req.body);
+    var user = {
+     first_name: req.body.first_name,
+     last_name: req.body.last_name,
+     email: req.body.email,
+     password: req.body.password,
+     confirm_password: req.body.confirm_password
+   };
+   console.log(user.password)
+   console.log(user.confirm_password)
+    if(user.password != user.confirm_password) {
+      console.log("Passwords do not match")
+      res.redirect('/signup')
+    }
+    else {
+      console.log("Passwords match")
+      res.redirect('/')
+    }
+
+  });
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
